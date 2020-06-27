28fec349ef53f36e2a435daf6c03c8623b59f5c5
Nick S <shupsta2010@gmail.com>
2020-06-27 14:02:32 -0700

    scroll play scene background

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -12,12 +12,19 @@ class Play extends Phaser.Scene {
     }
 
     create() {
+        //place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+        //place white rectangle boarder
         this.add.rectangle(5, 5, 630, 32, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(5, 443, 630, 32, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(5, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
         // green UI background
         this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
+    }
+
+    update() {
+        this.starfield.tilePositionX -= 4;
     }
 
 }
