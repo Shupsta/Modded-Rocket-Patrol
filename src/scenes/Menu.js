c9c3f5ad53110ccc8640d61a5452ac53857fc7d4
Nick S <shupsta2010@gmail.com>
2020-06-27 17:18:29 -0700

    expanded menu scene, made partly functional

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -15,8 +15,36 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(20, 20, "Rocket Patrol Menu");
-        this.scene.start("playScene");
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+
+        }
+
+        //show menu text
+        let centerX = game.config.width/2;
+        let centerY = game.config.height/2;
+        let textSpacer = 64;
+
+        this.add.text(centerX, centerY - textSpacer, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY, 'Use arrows to move and (F) to Fire', menuConfig).setOrigin(0.5);
+        menuConfig.backgroundColor = '#00FF00';
+        menuConfig.color = '#000';
+        this.add.text(centerX, centerY + textSpacer, 'Press <- for easy and -> for Hard', menuConfig).setOrigin(0.5);
+        
+        //define keys
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        // this.add.text(20, 20, "Rocket Patrol Menu");
+        // this.scene.start("playScene");
     }
 
 }
